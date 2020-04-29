52f71c5b7c7d6f88b49f92ab75147605b8ef437f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-29 08:25:22 +0000

    Deploy Lucas-C/Nonogram to github.com/Lucas-C/Nonogram.git:gh-pages

--- generate_grids_list.js
+++ generate_grids_list.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
+const dirPathPrefix = process.argv[2];
 
-fs.readdir('grids/', function (err, files) {
-    fs.writeFileSync('docs/grids-files.json', JSON.stringify(files));
+fs.readdir('grids/', function (err, filenames) {
+    fs.writeFileSync('docs/grids-files.json', JSON.stringify(filenames.map(function(filename) { return dirPathPrefix + filename; })));
 });
