e69308a51518c17e7ab625823be4cf8fd9eab22d
Lucas-C <Lucas-C@users.noreply.github.com>
2022-05-01 14:27:06 +0000

    deploy: 4c5c83b2222dac3ccf1dca1fe02f0090e7b14ba4

--- generate_grids_list.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const dirPathPrefix = process.argv[2];
-
-fs.readdir('grids/', function (err, filenames) {
-    fs.writeFileSync('docs/grids-files.json', JSON.stringify(filenames.map(function(filename) { return dirPathPrefix + filename; })));
-});
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import Solver from './Solver'
-import Editor from './Editor'
-import Game from './Game'
-
-export { Solver, Editor, Game }
